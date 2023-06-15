45d3d275a36c176fce2af4184afec1ea637836d6
tudormardare <tudor.mardare@stud.unifi.it>
2023-06-15 13:50:10 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Jump.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Jump.dir/main.cpp.obj"
+  "Jump.exe"
+  "Jump.exe.manifest"
+  "Jump.pdb"
+  "libJump.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Jump.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Jump.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Jump.
+# This may be replaced when dependencies are built.
