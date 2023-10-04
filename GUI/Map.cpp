9d990163398d9c21e8d34358a9a167af8613de36
denisjaupi <denis.jaupi@edu.unifi.it>
2023-10-04 15:53:05 +0200

    Creazione classe Map e CollisionManager.cpp. Logica della funzione handleWindowBoundsCollisions. Manca logica della funzione handlePlatformCollisions.

--- GUI/Map.cpp
+++ GUI/Map.cpp
@@ -1,5 +1,63 @@
-//
-// Created by Denis Jaupi on 26/09/23.
-//
-
 #include "Map.h"
+
+Map::Map() {
+
+    loadTexture(GAME_FOREGROUND_PATH);
+    sprite.setTexture(texture);
+    size = texture.getSize();
+    createPlatformBounds();
+
+}
+
+void Map::loadTexture(const std::string& filePath) {
+
+    if (!texture.loadFromFile(filePath)) {
+        std::cout << "Errore: Impossibile caricare la texture dal file " << filePath << std::endl;
+    }
+}
+
+void Map::applyScale() {
+    float scaleX = static_cast<float>(WINDOW_WIDTH) / static_cast<float>(texture.getSize().x);
+    float scaleY = static_cast<float>(WINDOW_HEIGHT) / static_cast<float>(texture.getSize().y);
+    sprite.setScale(scaleX, scaleY);
+}
+
+void Map::drawSprite(sf::RenderWindow& window) {
+    window.draw(sprite);
+}
+
+void Map::createPlatformBounds() {
+    sf::Image image = texture.copyToImage();
+
+    for (unsigned int y = 0; y < size.y; ++y) {
+        for (unsigned int x = 0; x < size.x; ++x) {
+            sf::Color pixelColor = image.getPixel(x, y);
+
+            if (pixelColor == PLATFORM_COLOR) {
+                // Trova la larghezza e l'altezza effettive della piattaforma
+                unsigned int width = 1;
+                unsigned int height = 1;
+
+                // Espandi la larghezza
+                while (x + width < size.x && texture.copyToImage().getPixel(x + width, y) == PLATFORM_COLOR)
+                    width++;
+
+                // Espandi l'altezza
+                while (y + height < size.y && texture.copyToImage().getPixel(x, y + height) == PLATFORM_COLOR)
+                    height++;
+
+                // Crea una bounding box basata sulle dimensioni trovate
+                platformBounds.emplace_back(x * 32.f, y * 32.f, width * 32.f, height * 32.f);
+
+                // Salta i pixel già inclusi nella bounding box
+                x += width - 1;
+            }
+        }
+    }
+}
+
+
+void Map::render(sf::RenderWindow &window) {
+    applyScale();
+    drawSprite(window);
+}
--- GUI/Map.h
+++ GUI/Map.h
@@ -6,9 +6,46 @@
 #define JUMP_MAP_H
 
 
-class Map {
+#include <SFML/Graphics.hpp>
+#include "Player.h"
+#include "../Controllers/CollisionManager.h"
+#include <iostream>
+#include <queue>
+#define WINDOW_WIDTH 1080
+#define WINDOW_HEIGHT 675
+#define GAME_FOREGROUND_PATH "PNG/Map-platforms/Map.png"
+
+
+
+class Map{
+
+private:
+
+    sf::Texture texture;
+    sf::Sprite sprite;
+    sf::Vector2u size;
+    std::vector<sf::FloatRect> platformBounds;
+    const sf::Color PLATFORM_COLOR = sf::Color(1, 27, 35);
+
+public:
+
+    Map();
+
+
+    void loadTexture(const std::string& filePath);
+
+    void applyScale();
+
+    void drawSprite(sf::RenderWindow& window);
+
+    void createPlatformBounds();
+
+
+    void render(sf::RenderWindow& window);
 
 };
+
+
 
 
 #endif //JUMP_MAP_H
