826cc31d486d3d04c6b7f03f1752f4d6e9be6e14
tudormardare <tudor.mardare@stud.unifi.it>
2024-02-05 18:39:10 +0100

    Rimmosso deltaTime da PhyscSystem

--- Utilities/PhysicsSystem.cpp
+++ Utilities/PhysicsSystem.cpp
@@ -2,18 +2,17 @@
 // Created by tudor on 04/10/2023.
 //
 
-#include <iostream>
 #include "PhysicsSystem.h"
 
 const float PhysicsSystem::GRAVITY = 3.f;
 
-void PhysicsSystem::applyGravity(Entity& entity, float deltaTime) {
+void PhysicsSystem::applyGravity(Entity& entity) {
     float verticalAcceleration = entity.getAcceleration().y;
     verticalAcceleration += GRAVITY ;
     entity.setAcceleration(sf::Vector2f(entity.getAcceleration().x, verticalAcceleration));
 }
 
-void PhysicsSystem::standOn(Entity& entity, float deltaTime) {
+void PhysicsSystem::standOn(Entity& entity) {
     float verticalAcceleration = entity.getAcceleration().y;
     verticalAcceleration -= GRAVITY;
     entity.setAcceleration(sf::Vector2f(entity.getAcceleration().x, verticalAcceleration));
